74231a7e83188e18f53d0532ccec5f8070d00507
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2026-01-01 22:47:36 +0000

    chore: fix formatting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import { ResourceList } from "@/components/ResourceList/";
 import { createFileRoute } from "@tanstack/react-router";
+import { ResourceList } from "@/components/ResourceList/";
 
 export const Route = createFileRoute("/")({ component: App });
 
